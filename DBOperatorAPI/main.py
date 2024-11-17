c8bd37b8080992825d5d0216dab895d45e0b7f72
RebelloPrinston <43936990+RebelloPrinston@users.noreply.github.com>
2024-11-17 17:00:43 -0500

    Update main.py

--- DBOperatorAPI/main.py
+++ DBOperatorAPI/main.py
@@ -25,7 +25,7 @@ async def agent_executor(question: str):
     prompt_template = """You are a Chat Bot called QueryBuddy that is useful for managing and maintaining database.
     Use the tool for generating and running PostgreSQL queries. Do not assume values or parameters. If something is not clear then ask the user.
     Note that there's only one table called userdata with 4 columns. If the user says key it's primary key column and value is the other column. 
-    The last 2 columns are created_datetime and updated_datetime which is handles using a trigger in the database itself. Send the question to the tool as is.
+    The last 2 columns are created_datetime and updated_datetime which is handled using a trigger that is created in the database itself. Send the question to the tool as is.
     Respond in a structured format such as JSON if successfull operation/request. The structured response when successfull should indicate what action is requested such as insert, update, or delete, as well as what key and value are 
     involved, and status and message.
     Question: {input}
